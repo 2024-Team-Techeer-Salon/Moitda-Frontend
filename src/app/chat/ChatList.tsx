2284961c674d8332d7a61f326a9759a2885723e3
j._.jinoo <jinoo0306@naver.com>
2024-05-04 19:22:43 +0900

    design : 채팅 리스트 디자인 및 하드 코딩 일부 수정

--- src/app/chat/ChatList.tsx
+++ src/app/chat/ChatList.tsx
@@ -39,9 +39,9 @@ function ChatElement({
   unreadCount,
 }: chatListProps) {
   return (
-    <div className="flex w-full cursor-pointer flex-row items-center justify-start p-2 hover:bg-zinc-50">
+    <div className="flex w-full cursor-pointer flex-row items-center justify-center py-2 hover:bg-zinc-50">
       {/* 대표 이미지 */}
-      <div className="m-2 flex h-12 w-12">
+      <div className="m-4 flex h-12 w-12">
         <Image
           src={titleImage}
           width={1024}
@@ -51,19 +51,23 @@ function ChatElement({
         />
       </div>
 
-      <div className="m-2 flex h-full w-2/3 flex-col items-start justify-center">
+      {/* 채팅방 정보 */}
+      <div className="flex h-full w-2/3 flex-col items-start justify-center p-2">
+        {/* 정보 윗 줄 : 채팅방 이름, 인원수, 시간 */}
         <div className="mb-2 flex w-full items-center justify-start">
           <div className="flex w-2/3 items-center justify-start">
             <span className="text-l truncate font-bold">{title}</span>
             <span className="text-md ml-2 text-zinc-400">{peopleCount}</span>
           </div>
-          <div className="ml-2 flex w-1/3 justify-end text-right text-sm text-zinc-400">
+          <div className="flex w-1/3 justify-end p-2 text-right text-sm text-zinc-400">
             {lastTimeStr(lastTime)}
           </div>
         </div>
+
+        {/* 정보 아랫 줄 : 마지막 채팅, 안 읽은 채팅 수 */}
         <div className="flex w-full items-center justify-start">
-          <span className="flex w-5/6 text-zinc-400">{lastChat}</span>
-          <span className="ml-2 flex h-6 w-6 items-center justify-center rounded-full bg-black text-[10px] text-white">
+          <span className="w-5/6 truncate pr-8 text-zinc-400">{lastChat}</span>
+          <span className="flex h-6 w-6 items-center justify-center rounded-full bg-black text-[10px] text-white">
             {unreadCountStr(unreadCount)}
           </span>
         </div>
@@ -84,19 +88,19 @@ function ChatList() {
         unreadCount={1}
       />
       <ChatElement
-        titleImage="https://i.ibb.co/0GtvPDT/Kakao-Talk-Photo-2024-04-17-21-26-58.jpg"
+        titleImage="https://i.ibb.co/556bLCN/image.png"
         title="새로 나온 와퍼 먹으러 가 볼 사람"
-        peopleCount={3}
+        peopleCount={8}
         lastTime="2024-05-03 18:00:58"
-        lastChat="그럼 그 때 봬요!"
+        lastChat="후추가 좀 더 있었으면 좋겠다 향이 조금 아쉬워"
         unreadCount={62}
       />
       <ChatElement
-        titleImage="https://i.ibb.co/0GtvPDT/Kakao-Talk-Photo-2024-04-17-21-26-58.jpg"
-        title="모각코 할 사람!"
-        peopleCount={3}
+        titleImage="https://i.ibb.co/KDg6p1L/image.png"
+        title="배드민턴 치러갈 사람"
+        peopleCount={16}
         lastTime="2024-05-02 18:00:58"
-        lastChat="그럼 그 때 봬요!"
+        lastChat="자리는 어떡하죠?"
         unreadCount={990}
       />
     </div>
