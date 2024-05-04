42ea2f8d8082081f80da720169a7ae68ec93ba69
j._.jinoo <jinoo0306@naver.com>
2024-05-04 16:42:15 +0900

    design : 카테고리 검색 결과 페이지 퍼블리싱

--- src/app/search/category/[keyword]/page.tsx
+++ src/app/search/category/[keyword]/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+import Image from 'next/image';
 import Body from './Body.tsx';
 
 async function page(props: any) {
@@ -7,6 +8,13 @@ async function page(props: any) {
     <div className="flex h-full w-full flex-col items-center justify-center">
       <div className="flex h-full w-[67.5rem] flex-col items-center justify-start pt-8">
         <div className="flex w-full flex-row items-center justify-start text-2xl">
+          <Image
+            src="https://i.ibb.co/qNrTSH7/free-icon-exercise-7750230.png"
+            width={128}
+            height={128}
+            alt="arrow-left"
+            className="m-6 flex rounded-full border border-gray-300"
+          />
           <span className="text-4xl font-bold">#{searchKeyword}</span>
         </div>
         <div className="flex h-full w-full flex-col items-center justify-center">
