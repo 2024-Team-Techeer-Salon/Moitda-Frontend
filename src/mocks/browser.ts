baa59e3461f43f71e41bd1f17e634f284d4c76fd
j._.jinoo <jinoo0306@naver.com>
2024-05-05 00:27:48 +0900

    chore : MSW 라이브러리 삭제

--- src/mocks/browser.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable import/extensions */
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
-
-export const worker = setupWorker(...handlers);
--- src/mocks/handlers.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable import/no-extraneous-dependencies */
-import { HttpResponse, http } from 'msw';
-
-export const handlers = [
-  http.get('/api/v1/tracks/track_id', () => HttpResponse.json({})),
-];
