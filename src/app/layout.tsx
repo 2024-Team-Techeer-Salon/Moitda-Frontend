0dd94b0916cdcd79f3490b58aaa41cb4bfc6ebe0
j._.jinoo <jinoo0306@naver.com>
2024-05-06 22:53:19 +0900

    chore : React-Query 라이브러리 개발 환경 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './styles/globals.css';
 import StoreProvider from './StoreProvider.tsx';
 import Header from './components/Header.tsx';
 import Footer from './components/Footer.tsx';
+import QueryProviders from '../lib/QueryProvider.tsx';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,7 +28,9 @@ export default function RootLayout({
           sizes="any"
         />
         <Header />
-        <StoreProvider>{children}</StoreProvider>
+        <QueryProviders>
+          <StoreProvider>{children}</StoreProvider>
+        </QueryProviders>
         <Footer />
       </body>
     </html>
