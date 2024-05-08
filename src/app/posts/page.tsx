f6ceb316fc943c2e7c1e4f9a2bcfab663383b659
j._.jinoo <jinoo0306@naver.com>
2024-05-08 19:13:32 +0900

    feat : GPS 기능으로 현재 위치 표시 구현 완료

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,20 +1,23 @@
+/* eslint-disable no-undef */
+/* eslint-disable @next/next/no-script-component-in-head */
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-/* eslint-disable no-trailing-spaces */
+/* eslint-disable operator-linebreak */
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-confusing-arrow */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable react/display-name */
-/* eslint-disable no-param-reassign */
-/* eslint-disable react-hooks/rules-of-hooks */
 
 'use client';
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Script from 'next/script';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import dayjs from 'dayjs';
@@ -30,6 +33,7 @@ import { DateTimePicker } from '@mui/x-date-pickers';
 import 'react-quill/dist/quill.snow.css';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
+import Head from 'next/head';
 
 const ReactQuill = dynamic(async () => import('react-quill'), {
   ssr: false,
@@ -114,6 +118,31 @@ function page() {
   const [images, setImages] = useState<string[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
+  const [address, setAddress] = useState('');
+  const [meetingAddressModalOpen, setMeetingAddressModalOpen] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const today = dayjs();
+  const [center, setCenter] = useState({ lat: 0, lng: 0 });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // 사용자 위치를 성공적으로 가져온 경우, 지도 중심을 사용자의 현재 위치로 설정
+          setCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error(`Error Code = ${error.code} - ${error.message}`);
+        },
+      );
+      // console.log(center);
+    } else {
+      console.error('이 브라우저에서는 Geolocation이 지원되지 않습니다.');
+    }
+  }, []);
 
   const handleImageChange = (e: any) => {
     const { files } = e.target;
@@ -137,9 +166,6 @@ function page() {
     }
   };
 
-  const { register, handleSubmit } = useForm();
-  const today = dayjs();
-
   const onSubmit = (data: any) => {
     console.log(data); // 이곳에서 form 데이터를 처리합니다.
   };
@@ -157,8 +183,74 @@ function page() {
     setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
 
+  // 주소 검색 모달
+  function DaumPostCodeModal() {
+    const handleComplete = (data: any) => {
+      let fullAddress = data.buildingName;
+      let extraAddress = '';
+
+      if (data.addressType === 'R') {
+        if (data.bname !== '') {
+          extraAddress += data.bname;
+        }
+        if (data.buildingName !== '') {
+          extraAddress +=
+            extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+        }
+        fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+      }
+
+      window.kakao.maps.load(() => {
+        const geocoder = new kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(data.address, (result, status) => {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const newCenter = {
+              lat: result[0].y,
+              lng: result[0].x,
+            };
+            setCenter({
+              lat: parseFloat(newCenter.lat),
+              lng: parseFloat(newCenter.lng),
+            });
+          }
+        });
+      });
+
+      console.log(data); // 콘솔에 주소 출력
+      setAddress(fullAddress); // 상태에 주소 저장
+      setMeetingAddressModalOpen(false); // 모달 닫기
+    };
+
+    return (
+      <div
+        className="fixed inset-0 z-40 flex h-full w-full items-center justify-center bg-black bg-opacity-10"
+        onClick={() => setMeetingAddressModalOpen(false)}
+      >
+        <Script
+          type="text/javascript"
+          src={process.env.NEXT_PUBLIC_KAKAO_SDK_URL}
+        />
+        <div className="flex h-96 w-1/3">
+          <DaumPostcode
+            onComplete={handleComplete}
+            autoClose={true}
+            className="h-full w-full"
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="mt-20 flex h-full w-full flex-col items-center justify-center">
+      <Head>
+        <script
+          type="text/javascript"
+          src={process.env.NEXT_PUBLIC_KAKAO_SDK_URL}
+        />
+      </Head>
+
       <div className="flex h-full w-[67.5rem] flex-col">
         {/* 제목 입력 및 카테고리 선택 */}
         <div className="flex h-12 w-full flex-row ">
@@ -192,15 +284,23 @@ function page() {
 
         {/* 장소 선택 */}
         <div className="mt-8 flex h-60 w-full flex-row">
+          {meetingAddressModalOpen && <DaumPostCodeModal />}
           <div className="flex w-1/2 flex-col justify-end ">
             <p className="flex text-sm text-zinc-300">
               모임 장소를 입력해 주세요!
             </p>
             <div className="mt-4 flex h-12 w-full flex-row">
-              <p className="border-1 mr-4  flex w-full items-center justify-start border border-zinc-300 pl-2 text-zinc-300">
-                주소
-              </p>
-              <button className="btn h-12 w-32 bg-[#E6E1E1] text-white hover:bg-[#C7B7B7]">
+              <input
+                className="border-1 mr-4 flex w-full items-center justify-start border border-zinc-300 pl-2 focus:outline-none"
+                readOnly
+                type="text"
+                value={address}
+                placeholder="주소"
+              />
+              <button
+                className="btn h-12 w-32 bg-[#E6E1E1] text-white hover:bg-[#C7B7B7]"
+                onClick={() => setMeetingAddressModalOpen(true)}
+              >
                 주소 검색
               </button>
             </div>
@@ -218,10 +318,12 @@ function page() {
               src={process.env.NEXT_PUBLIC_KAKAO_SDK_URL}
               strategy="beforeInteractive"
             />
-            <Map
-              center={{ lat: 33.450701, lng: 126.570667 }}
+            <Map // 지도를 표시할 컨테이너
+              center={center} // 지도의 중심좌표
               style={{ width: '100%', height: '100%' }}
-            ></Map>
+            >
+              <MapMarker position={center} /> {/* 사용자의 위치에 마커 표시 */}
+            </Map>
           </div>
         </div>
 
