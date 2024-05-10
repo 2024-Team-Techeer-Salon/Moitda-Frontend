e820c79b9b9984b370f14ac2af8be4a5c0e3af1f
j._.jinoo <jinoo0306@naver.com>
2024-05-11 00:29:46 +0900

    chore : axios 세팅

---
+++ src/api/sample.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable consistent-return */
+/* eslint-disable import/prefer-default-export */
+import { api } from './axios.config.ts';
+
+export async function sample() {
+  try {
+    const response = await api.get('/users/100');
+    return response.data;
+  } catch (error) {
+    console.error('sample error : ', error);
+  }
+}
