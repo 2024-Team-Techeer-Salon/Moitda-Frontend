3f3ffd4945fe1f04c1dda253d80976b3a809990b
j._.jinoo <jinoo0306@naver.com>
2024-05-16 16:42:15 +0900

    feat : 홈으로 리다이렉트 방식 변경

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,6 @@ export const images = {
   domains: ['i.ibb.co'],
 };
 
-export async function redirects() {
-  return [
-    {
-      source: '/',
-      destination: '/home',
-      permanent: true,
-    },
-  ];
-}
-
 export default {
   images,
-  redirects,
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from 'next/navigation';
 
 export default function page() {
   redirect('/home');
-  // permanentRedirect('/home');
-  redirect('/review');
+
   return <div>Redirecting...</div>;
 }
