d9afaf1b51e18bff9f3523ce40d02756e3ab242b
j._.jinoo <jinoo0306@naver.com>
2024-06-27 05:05:36 +0900

    feat : 쿠키에서 options 속성 제거

--- src/app/cookies.tsx
+++ src/app/cookies.tsx
@@ -1,21 +1,15 @@
 /* eslint-disable implicit-arrow-linebreak */
 import { Cookies } from 'react-cookie';
-import { CookieSetOptions } from 'universal-cookie';
 
 const cookies = new Cookies();
 
 // 쿠키를 저장함
-export const setCookie = (
-  name: string,
-  value: string,
-  option: CookieSetOptions,
-) => {
-  cookies.set(name, value, { ...option });
+export const setCookie = (name: string, value: string) => {
+  cookies.set(name, value);
 };
 
 // 쿠키를 가져옴
 export const getCookie = (name: string) => cookies.get(name);
 
 // 쿠키를 삭제함
-export const removeCookie = (name: string, option: CookieSetOptions) =>
-  cookies.remove(name, { ...option });
+export const removeCookie = (name: string) => cookies.remove(name);
